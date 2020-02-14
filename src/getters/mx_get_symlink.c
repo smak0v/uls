528299211c74f6ec86339f9aa5421ad85fb366c0
Grom1799 <gromfighter@gmail.com>
2020-02-14 14:34:31 +0200

    Fixed bug with symlinks

--- src/getters/mx_get_symlink.c
+++ src/getters/mx_get_symlink.c
@@ -3,10 +3,11 @@
 char *mx_get_symlink(t_data * data) {
     char *symlink = NULL;
     ssize_t size = 0;
+    int tmp = data->file_size > 0 ? data->file_size + 1 : 100;
 
     if (MX_IS_LNK(data->mode)) {
         symlink = mx_strnew(data->file_size);
-        size = readlink(data->full_filename, symlink, data->file_size + 1);
+        size = readlink(data->full_filename, symlink, tmp);
         if (size > 0)
             return symlink;
         mx_strdel(&symlink);
